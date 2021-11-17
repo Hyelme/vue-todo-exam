0bff3d1c4fec1befb2ff2cd626e8a2b5f5e269b5
Hyelme <jhrim0810@naver.com>
2021-11-17 17:51:46 +0900

    [ADD]change vue file

---
+++ src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <ul>
+        <li v-for="item in todoItem" :key="item">{{ item }}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todoItem: []
+    }
+  },
+  created() {
+    if(localStorage.length > 0) {
+      for(var i = 0; i<localStorage.length; i++) {
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+          this.todoItem.push(localStorage.key(i));
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
