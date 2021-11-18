e333b67f53952c2c0d80de25f030c9c6f367878d
Hyelme <jhrim0810@naver.com>
2021-11-18 09:32:29 +0900

    [UPDATE] TodoList 컴포넌트 UI 스타일링

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
       <ul>
-        <li v-for="item in todoItem" :key="item">{{ item }}</li>
+        <li v-for="item in todoItem" :key="item" class="shadow">
+          {{ item }}
+          <span class="removeBtn" @click="removeTodo">
+            <i class="fas fa-trash"></i>
+          </span>
+        </li>
       </ul>
   </div>
 </template>
@@ -11,6 +16,11 @@ export default {
   data() {
     return {
       todoItem: []
+    }
+  },
+  methods: {
+    removeTodo(){
+
     }
   },
   created() {
@@ -26,5 +36,41 @@ export default {
 </script>
 
 <style>
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
 
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+
+  .checkBtnCompleted {
+    color: #b3adad;
+  }
+
+  .textCompleted {
+    text-decoration: line-through;
+    color: #b3adad;
+  }
+
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
 </style>
